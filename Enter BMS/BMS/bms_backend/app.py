3fc5ffa52b0c4a689e25b19e9dda07bbe74ba593
Fakhrul Islam <mislam2430230@bscse.uiu.ac.bd>
2025-12-12 01:18:24 +0600

    Update files

--- Enter BMS/BMS/bms_backend/app.py
+++ Enter BMS/BMS/bms_backend/app.py
@@ -1,3 +1,7 @@
+import eventlet
+eventlet.monkey_patch()
+
+
 from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from werkzeug.security import generate_password_hash, check_password_hash
